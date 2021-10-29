a662fb64606e1b15064e1c3791cba6d66c03209a
Amitava Chakrabarti <f20200010@goa.bits-pilani.ac.in>
2021-10-30 02:12:44 +0530

    Basic structure of Piece class

--- src/models/Piece.java
+++ src/models/Piece.java
@@ -1,5 +1,31 @@
 package models;
 
-public class Piece {
+import java.util.List;
+
+public abstract class Piece {
+
+    private final int rank;
+    private final int file;
+    private final String color;
+
+    protected Piece(int rank, int file, String color) {
+        this.rank = rank;
+        this.file = file;
+        this.color = color;
+    }
+
+    public int getRank() {
+        return this.rank;
+    }
+
+    public int getFile() {
+        return this.file;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public abstract List getAllMoves(Square[][] board);
     
 }
