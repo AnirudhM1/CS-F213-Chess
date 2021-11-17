056a0334e4c280bc830ac8280f297310d91d3f1d
Anirudh Mukherjee <f20200970@goa.bits-pilani.ac.in>
2021-11-17 23:19:22 +0530

    Updater class is now able to display gui on start
    Added start method to Updater class
    
    Updater class runs start when the gui needs to be displayed

--- src/controllers/Updater.java
+++ src/controllers/Updater.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Square;
 import views.Gui;
 
 public class Updater {
@@ -11,7 +12,25 @@ public class Updater {
     }
 
     private void init() {
-        board = null; // Default board
         gui = new Gui(this);
+        board = null;
+    }
+
+    public void start() {
+        board = null; // Default board
+        gui.setState(this, defaultBoard()); // We are using defaultBoard only for debugging purposes
+        gui.run();
+    }
+
+    // This function is only used for debugging purposes.
+    // This is to be deleted when the initialize function is completed in BC
+    private static Square[][] defaultBoard() {
+        Square[][] board = new Square[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                board[i][j] = Square.createSquare(i, j, null);
+            }
+        }
+        return board;
     }
 }
