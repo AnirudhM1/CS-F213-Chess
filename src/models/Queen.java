461f4325fffc8e085c6f668b65a74ea730865c32
Saahil Mittal <77740870+SaahilMittal@users.noreply.github.com>
2021-11-19 12:26:22 +0530

    Add files via upload

---
+++ src/models/Queen.java
@@ -0,0 +1,98 @@
+package models;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+
+public class Queen extends Piece{
+	protected Queen(int rank, int file, String color) {
+		super(rank, file, color);
+	}
+	
+	public boolean Check(Square End) {
+		if(End.getRank()>=0 && End.getFile()>=0 && End.getRank()<=7 && End.getFile()<=7) {
+			if(End.isOccupied()) {
+				if(this.getColor().equals(End.getPiece().getColor())) {
+					return false;
+				}
+				else {
+					return true;
+				}
+				
+			}
+			else {
+				return true;
+			}
+		}
+		else {
+			return false;
+		}
+	}
+	public List<Move> getAllMoves(Square[][] board){
+		List<Move> legalMoves = new ArrayList<>();
+		Square startSquare = createSquare(super.getRank(), super.getFile(),this);
+        Square endSquare = board[super.getRank()][super.getFile()];
+
+		for(int i = 0 ; i<7; i++)
+		{
+			endSquare =  board[super.getRank()+1][super.getFile()];
+			if(Check(endSquare)){
+				legalMoves.add(new Move(startSquare, endSquare));
+			}
+		}
+		for(int i = 0 ; i<7; i++)
+		{
+			endSquare =  board[super.getRank()-1][super.getFile()];
+			if(Check(endSquare)){
+				legalMoves.add(new Move(startSquare, endSquare));
+			}
+		}
+		for(int i = 0 ; i<7; i++)
+		{
+			endSquare =  board[super.getRank()][super.getFile()-1];
+			if(Check(endSquare)){
+				legalMoves.add(new Move(startSquare, endSquare));
+			}
+		}
+		for(int i = 0 ; i<7; i++)
+		{
+			endSquare =  board[super.getRank()+1][super.getFile()+1];
+			if(Check(endSquare)){
+				legalMoves.add(new Move(startSquare, endSquare));
+			}
+		}
+		for(int i = 0 ; i<7; i++)
+		{
+			endSquare =  board[super.getRank()+1][super.getFile()+1];
+			if(Check(endSquare)){
+				legalMoves.add(new Move(startSquare, endSquare));
+			}
+		}
+		for(int i = 0 ; i<7; i++)
+		{
+			endSquare =  board[super.getRank()+1][super.getFile()-1];
+			if(Check(endSquare)){
+				legalMoves.add(new Move(startSquare, endSquare));
+			}
+		}
+		for(int i = 0 ; i<7; i++)
+		{
+			endSquare =  board[super.getRank()-1][super.getFile()+1];
+			if(Check(endSquare)){
+				legalMoves.add(new Move(startSquare, endSquare));
+			}
+		}
+		for(int i = 0 ; i<7; i++)
+		{
+			endSquare =  board[super.getRank()-1][super.getFile()-1];
+			if(Check(endSquare)){
+				legalMoves.add(new Move(startSquare, endSquare));
+			}
+		}
+
+	}
+	return legalMoves;
+
+    
+}
