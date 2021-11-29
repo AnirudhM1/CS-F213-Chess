e5b3cadfe1afaae2872215eb9ce6bec16cf07262
Anirudh Mukherjee <f20200970@goa.bits-pilani.ac.in>
2021-11-29 15:53:18 +0530

    Added a utility function to Move.java
    Added a function which checks if a particular piece has moved 2 steps
    in a particular direction

--- src/models/Move.java
+++ src/models/Move.java
@@ -24,6 +24,15 @@ public class Move {
         return endSquare.getPiece() instanceof King;
     }
 
+    // Utility function to check if piece has moved more than one step
+    // Useful for special moves
+    public boolean hasMovedTwoSteps() {
+        int rank_moved = Math.abs(endSquare.getRank() - endSquare.getFile());
+        int file_moved = Math.abs(endSquare.getRank() - endSquare.getFile());
+
+        return (rank_moved == 2) || (file_moved == 2);
+    }
+
     public boolean equals(Move moveObj) {
         return this.startSquare.equals(moveObj.startSquare) && this.endSquare.equals(moveObj.endSquare);
     }
