47b5707709853ff6d8441bba8b2f033e07034866
ayushpaine <ayushmt701@gmail.com>
2021-12-04 01:08:38 +0530

    Added buttons to gui

--- src/views/Gui.java
+++ src/views/Gui.java
@@ -3,6 +3,8 @@ package views;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 import controllers.Updater;
 import models.Move;
@@ -28,6 +30,7 @@ public class Gui {
     private Client client;
 
     // private Updater updater;
+    private Updater updater;
 
     public Gui() {
         setMainFrame();
@@ -35,7 +38,8 @@ public class Gui {
     }
 
     public Gui(Updater updater) {
-        // this.updater = updater;
+
+        this.updater = updater;
 
         setMainFrame();
         setPanels();
@@ -66,6 +70,9 @@ public class Gui {
         // optionPanel.setLayout();
         optionPanel.setBackground(Color.BLACK);
 
+        optionPanel.setLayout(new GridLayout(1, 5));
+        setOptionPanel();
+
         c.gridx = 0;
         c.gridwidth = 2;
         c.gridy = 0;
@@ -80,7 +87,7 @@ public class Gui {
 
         outerBoardPanel = new JPanel();
         outerBoardPanel.setLayout(new GridBagLayout());
-        outerBoardPanel.setBackground(Color.GRAY);
+        outerBoardPanel.setBackground(Color.BLACK);
 
         c.gridx = 0;
         c.gridwidth = 1;
@@ -98,7 +105,7 @@ public class Gui {
 
         extraPanel = new JPanel();
         // extraPanel.setLayout();
-        extraPanel.setBackground(Color.GRAY);
+        extraPanel.setBackground(Color.BLACK);
 
         c.gridx = 1;
         c.gridwidth = 1;
@@ -108,7 +115,7 @@ public class Gui {
         c.weightx = 1;
         c.weighty = 0.9;
 
-        mainframe.add(extraPanel, c);
+        // mainframe.add(extraPanel, c);
     }
 
     // Updater class calls this method to set the state of the gui
@@ -125,6 +132,73 @@ public class Gui {
     public void resetClicks() {
         boardPanel.resetClicks();
     }
+
+    public void setOptionPanel() {
+        JButton button = new JButton();
+        button.setBackground(new Color(59, 89, 182));
+        button.setForeground(Color.WHITE);
+        button.setFocusPainted(false);
+
+
+        button.addActionListener(e -> {
+            updater.start();
+        });
+
+        button.setText("Restart");
+        optionPanel.add(button);
+        
+        JButton connectButton = new JButton("Start remote game");
+        connectButton.setBackground(new Color(59, 89, 182));
+        connectButton.setForeground(Color.WHITE);
+        connectButton.setFocusPainted(false);
+
+        connectButton.addActionListener(e -> {
+            connectionFrame = new JFrame();
+            connectionFrame.setTitle("Connect to server");
+            connectionFrame.setSize(300, 100);
+            connectionFrame.setLocationRelativeTo(null);
+            // connectionFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            connectionFrame.setLayout(new GridLayout(3, 1));
+            connectionFrame.setBackground(Color.BLUE);
+            connectionFrame.setResizable(false);
+            connectionFrame.add(new JPanel());
+
+            JPanel ipPanel = new JPanel();
+            ipPanel.setLayout(new GridLayout(1, 2));
+            ipPanel.setBackground(Color.GRAY);
+
+            JLabel ipLabel = new JLabel("      Internet Protocol: ");
+            ipLabel.setBackground(Color.GRAY);
+            ipPanel.add(ipLabel);
+
+            JTextField ipField = new JTextField();
+            ipField.setBackground(Color.WHITE);
+            
+            ipField.addActionListener(event -> {
+                JTextField field = (JTextField) event.getSource();
+                String ip = field.getText();
+                client = new Client(ip, 5000, updater);
+                Thread t = new Thread(client);
+                t.start();
+                connectionFrame.dispose();
+            });
+
+            ipPanel.add(ipField);
+            // connectionPanel.add(ipPanel);
+            connectionFrame.add(ipPanel);
+            connectionFrame.add(new JPanel());
+            connectionFrame.setVisible(true);
+        });
+
+        optionPanel.add(connectButton);
+        optionPanel.add(new JPanel());
+        optionPanel.add(new JPanel());
+        optionPanel.add(new JPanel());
+
+        
+    }
+
+    
 
     public void addMove(Move move) {
         // TODO Add move to the move list
